0a8d9ec3b82b2a09ec20fad946f11c00e6819ff7
Melinda Ivanov <jns.melinda@gmail.com>
2020-07-09 19:15:45 -0700

    timer stops at 0 and don't start again

--- quiz.js
+++ quiz.js
@@ -1,18 +1,23 @@
 document.getElementById("startQuiz").addEventListener("click", startTime);
 const timeEl = document.getElementById("countDown");
 var secondsLeft = 5;
+var timerStarted = "true";
+
 
 function startTime() {
-
   // TODO: check if timer has started and do nothing if so
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
-    if(secondsLeft <= 0) {
-      clearInterval(timerInterval);
-      timeEl.textContent = "";
-    }
+  if (timerStarted === "true") {
+    var timerInterval = setInterval(function () {
 
-  }, 1000);
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+      if (secondsLeft <= 0) {
+        clearInterval(timerInterval);
+        timerStarted = "false";
+      }
+
+    }, 1000);
+  }
 }
