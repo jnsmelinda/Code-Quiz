6d992d47e0175e953830648d1e529abc7d60b550
Melinda Ivanov <jns.melinda@gmail.com>
2020-07-10 18:39:29 -0700

    formatting + game over

--- quiz.js
+++ quiz.js
@@ -1,48 +1,47 @@
 const questions = [
-  {
-    question: "123?",
-    answers: [
-      "a", "b", "c", "d"
-    ],
-    correctAnswer: 2
-  },
-  {
-    question: "abc?",
-    answers: [
-      "1", "2", "3", "4"
-    ],
-    correctAnswer: 0
-  }
+    {
+        question: "123?",
+        answers: [
+            "a", "b", "c", "d"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "abc?",
+        answers: [
+            "1", "2", "3", "4"
+        ],
+        correctAnswer: 0
+    }
 ]
 
 document.getElementById("startQuiz").addEventListener("click", startTime);
-const timeEl = document.getElementById("countDown");
-let body = document.body;
+const countDownElement = document.getElementById("countDown");
 let secondsLeft = 10;
 let timerStarted = false;
-
+let timerInterval;
+let score = 0;
 
 function startTime() {
-  if (timerStarted === false) {
-    timerStarted = true;
+    if (timerStarted === false) {
+        timerStarted = true;
 
-    displayQuestion(0);
+        displayQuestion(0);
 
-    let timerInterval = setInterval(function () {
+        timerInterval = setInterval(function () {
 
-
-      if (secondsLeft <= 5) {
-        timeEl.style.color = "red";
-      }
-      if (secondsLeft >= 0) {
-        timeEl.textContent = secondsLeft;
-        secondsLeft--;
-      }
-      else {
-        clearInterval(timerInterval);
-      }
-    }, 1000);
-  }
+            if (secondsLeft <= 5) {
+                countDownElement.style.color = "red";
+            }
+            if (secondsLeft >= 0) {
+                countDownElement.textContent = secondsLeft;
+                secondsLeft--;
+            }
+            else {
+                displayGameOver();
+            }
+        }, 1000);
+    }
 }
 
 function displayQuestion(questionIndex) {
@@ -50,13 +49,24 @@ function displayQuestion(questionIndex) {
     question.textContent = questions[questionIndex].question;
 
     for (let i = 0; i < 4; i++) {
-      const answer = document.getElementById(`answer-${i}`);
-      answer.textContent = questions[questionIndex].answers[i];
-      answer.onclick = () => markAnswer(questionIndex, i);
+        const answer = document.getElementById(`answer-${i}`);
+        answer.textContent = questions[questionIndex].answers[i];
+        answer.onclick = () => markAnswer(questionIndex, i);
     }
 }
 
 function markAnswer(questionIndex, answerIndex) {
-  alert(questions[questionIndex].correctAnswer === answerIndex ? "correct" : "incorrect");
-  displayQuestion(questionIndex + 1);
+    alert(questions[questionIndex].correctAnswer === answerIndex ? "correct" : "incorrect");
+    questionIndex++;
+    if (questionIndex === questions.length) {
+        displayGameOver();
+    }
+    else {
+        displayQuestion(questionIndex);
+    }
+}
+
+function displayGameOver() {
+    alert("game over");
+    clearInterval(timerInterval);
 }
